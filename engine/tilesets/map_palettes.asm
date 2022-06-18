4dcec1479ecd26175f361babe7430095a2430e08
SUGO-SG-13-DAMI\Damien <damien.doury@hotmail.fr>
2022-06-18 14:28:06 +0200

    [feature] Bed priority coded (no art yet).
    Figured out a code that always a few BG map tiles to have priority, without needing all map to switch to the new system, which would take more than 3 kB on the ROM.
    Kinda like the grass.
    Unoptimized code, and it will need more testing.

--- engine/tilesets/map_palettes.asm
+++ engine/tilesets/map_palettes.asm
@@ -5,40 +5,9 @@ _SwapTextboxPalettes::
 .loop
 	push bc
 	ld c, SCREEN_WIDTH
-.innerloop
-	ld a, [hl]
-	push hl
-	srl a
-	jr c, .UpperNybble
-	ld hl, wTilesetPalettes
-	add [hl]
-	ld l, a
-	ld a, [wTilesetPalettes + 1]
-	adc 0
-	ld h, a
-	ld a, [hl]
-	and $f
-	jr .next
 
-.UpperNybble:
-	ld hl, wTilesetPalettes
-	add [hl]
-	ld l, a
-	ld a, [wTilesetPalettes + 1]
-	adc 0
-	ld h, a
-	ld a, [hl]
-	swap a
-	and $f
+	call GetBGMapTilePalettes
 
-.next
-	pop hl
-	ld [de], a
-	res 7, [hl]
-	inc hl
-	inc de
-	dec c
-	jr nz, .innerloop
 	pop bc
 	dec b
 	jr nz, .loop
@@ -47,8 +16,12 @@ _SwapTextboxPalettes::
 _ScrollBGMapPalettes::
 	ld hl, wBGMapBuffer
 	ld de, wBGMapPalBuffer
+	; fallthrough
+
+GetBGMapTilePalettes:
 .loop
 	ld a, [hl]
+	push af
 	push hl
 	srl a
 	jr c, .UpperNybble
@@ -77,6 +50,43 @@ _ScrollBGMapPalettes::
 
 .next
 	pop hl
+
+
+
+
+
+	ld b, a; B contains the palette without priority added.
+
+	pop af
+	cp $34 		; Bed tile 1.
+	jr z, .priority_second_check
+	cp $35 		; Bed tile 2.
+	jr z, .priority_second_check
+	jr .done_with_priority
+
+.priority_second_check
+	ld a, [wMapGroup]
+	cp GROUP_GOLDENROD_HOSPITAL_ROOM
+	jr nz, .done_with_priority
+
+	ld a, [wMapNumber]
+	cp MAP_GOLDENROD_HOSPITAL_ROOM
+	jr nz, .done_with_priority
+
+	; Adding priority.
+	ld a, b
+	or $80
+	ld b, a
+	
+
+
+
+.done_with_priority
+	ld a, b
+
+
+
+
 	ld [de], a
 	res 7, [hl]
 	inc hl
