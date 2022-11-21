49f65b4e82fcc817a7a43df3fa475f71fbae4e77
Damien Doury <damien.doury@hotmail.fr>
2022-11-21 12:46:25 +0100

    [edit] LOTS of room made in engine/battle/core.asm !!!

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -175,7 +175,7 @@ WildFled_EnemyFled_LinkBattleCanceled:
 BattleTurn:
 	ldh a, [hInMenu]
 	push af
-	ld a, 1 ; or "xor a" for the value 0
+	xor a ;ld a, 1 ; or "xor a" for the value 0
 	ldh [hInMenu], a
 .loop
 	call Stubbed_Increments5_a89a
@@ -2717,7 +2717,7 @@ PlayVictoryMusic:
 
 .trainer_victory
 	ld de, MUSIC_GYM_VICTORY
-	call IsGymLeader
+	farcall IsGymLeader
 	jr c, .play_music
 	ld de, MUSIC_TRAINER_VICTORY
 
@@ -2727,22 +2727,6 @@ PlayVictoryMusic:
 .lost
 	pop de
 	ret
-
-IsKantoGymLeader:
-	ld hl, KantoGymLeaders
-	jr IsGymLeaderCommon
-
-IsGymLeader:
-	ld hl, GymLeaders
-IsGymLeaderCommon:
-	push de
-	ld a, [wOtherTrainerClass]
-	ld de, 1
-	call IsInArray
-	pop de
-	ret
-
-INCLUDE "data/trainers/leaders.asm"
 
 HandlePlayerMonFaint:
 	call FaintYourPokemon
@@ -8277,182 +8261,12 @@ StartBattle:
 
 	ld a, [wTimeOfDayPal]
 	push af
-	call BattleIntro
+	farcall BattleIntro
 	call DoBattle
 	farcall ExitBattle
 	pop af
 	ld [wTimeOfDayPal], a
 	scf
-	ret
-
-BattleIntro:
-	farcall StubbedTrainerRankings_Battles ; mobile
-	farcall ForceBattleAnimationStart
-	call LoadTrainerOrWildMonPic
-	xor a
-	ld [wTempBattleMonSpecies], a
-	ld [wBattleMenuCursorPosition], a
-	xor a
-	ldh [hMapAnims], a
-	farcall PlayBattleMusic
-	farcall ShowLinkBattleParticipants
-	farcall FindFirstAliveMonAndStartBattle
-	call DisableSpriteUpdates
-	farcall ClearBattleRAM
-	call InitEnemy
-	call BackUpBGMap2
-	ld b, SCGB_BATTLE_GRAYSCALE
-	call GetSGBLayout
-	ld hl, rLCDC
-	res rLCDC_WINDOW_TILEMAP, [hl] ; select vBGMap0/vBGMap2
-	call InitBattleDisplay
-	call BattleStartMessage
-	ld hl, rLCDC
-	set rLCDC_WINDOW_TILEMAP, [hl] ; select vBGMap1/vBGMap3
-	xor a
-	ldh [hBGMapMode], a
-	call EmptyBattleTextbox
-	hlcoord 9, 7
-	lb bc, 5, 11
-	call ClearBox
-	hlcoord 1, 0
-	lb bc, 4, 10
-	call ClearBox
-	call ClearSprites
-	ld a, [wBattleMode]
-	cp WILD_BATTLE
-	call z, UpdateEnemyHUD
-	ld a, $1
-	ldh [hBGMapMode], a
-	ret
-
-LoadTrainerOrWildMonPic:
-	ld a, [wOtherTrainerClass]
-	and a
-	jr nz, .Trainer
-	ld a, [wTempWildMonSpecies]
-	ld [wCurPartySpecies], a
-
-.Trainer:
-	ld [wTempEnemyMonSpecies], a
-	ret
-
-InitEnemy:
-	ld a, [wOtherTrainerClass]
-	and a
-	jp nz, InitEnemyTrainer ; trainer
-	jp InitEnemyWildmon ; wild
-
-BackUpBGMap2:
-	ldh a, [rSVBK]
-	push af
-	ld a, BANK(wDecompressScratch)
-	ldh [rSVBK], a
-	ld hl, wDecompressScratch
-	ld bc, $40 tiles ; vBGMap3 - vBGMap2
-	ld a, $2
-	call ByteFill
-	ldh a, [rVBK]
-	push af
-	ld a, $1
-	ldh [rVBK], a
-	ld de, wDecompressScratch
-	hlbgcoord 0, 0 ; vBGMap2
-	lb bc, BANK(BackUpBGMap2), $40
-	call Request2bpp
-	pop af
-	ldh [rVBK], a
-	pop af
-	ldh [rSVBK], a
-	ret
-
-InitEnemyTrainer:
-	ld [wTrainerClass], a
-	farcall StubbedTrainerRankings_TrainerBattles
-	xor a
-	ld [wTempEnemyMonSpecies], a
-	callfar GetTrainerAttributes
-	callfar ReadTrainerParty
-
-	; RIVAL1's first mon has no held item
-	ld a, [wTrainerClass]
-	cp RIVAL1
-	jr nz, .ok
-	xor a
-	ld [wOTPartyMon1Item], a
-
-.ok
-	ld de, vTiles2
-	callfar GetTrainerPic
-	xor a
-	ldh [hGraphicStartTile], a
-	dec a
-	ld [wEnemyItemState], a
-	hlcoord 12, 0
-	lb bc, 7, 7
-	predef PlaceGraphic
-	ld a, -1
-	ld [wCurOTMon], a
-	ld a, TRAINER_BATTLE
-	ld [wBattleMode], a
-
-	call IsGymLeader
-	jr nc, .done
-	xor a
-	ld [wCurPartyMon], a
-	ld a, [wPartyCount]
-	ld b, a
-.partyloop
-	push bc
-	ld a, MON_HP
-	call GetPartyParamLocation
-	ld a, [hli]
-	or [hl]
-	jr z, .skipfaintedmon
-	ld c, HAPPINESS_GYMBATTLE
-	callfar ChangeHappiness
-.skipfaintedmon
-	pop bc
-	dec b
-	jr z, .done
-	ld hl, wCurPartyMon
-	inc [hl]
-	jr .partyloop
-.done
-	ret
-
-InitEnemyWildmon:
-	ld a, WILD_BATTLE
-	ld [wBattleMode], a
-	farcall StubbedTrainerRankings_WildBattles
-	call LoadEnemyMon
-	ld hl, wEnemyMonMoves
-	ld de, wWildMonMoves
-	ld bc, NUM_MOVES
-	call CopyBytes
-	ld hl, wEnemyMonPP
-	ld de, wWildMonPP
-	ld bc, NUM_MOVES
-	call CopyBytes
-	ld hl, wEnemyMonDVs
-	predef GetUnownLetter
-	ld a, [wCurPartySpecies]
-	cp UNOWN
-	jr nz, .skip_unown
-	ld a, [wFirstUnownSeen]
-	and a
-	jr nz, .skip_unown
-	ld a, [wUnownLetter]
-	ld [wFirstUnownSeen], a
-.skip_unown
-	ld de, vTiles2
-	predef GetAnimatedFrontpic
-	xor a
-	ld [wTrainerClass], a
-	ldh [hGraphicStartTile], a
-	hlcoord 12, 0
-	lb bc, 7, 7
-	predef PlaceGraphic
 	ret
 
 FillEnemyMovesFromMoveIndicesBuffer: ; unreferenced
@@ -9282,87 +9096,4 @@ CopyBackpic:
 	ld e, a
 	dec b
 	jr nz, .outer_loop
-	ret
-
-BattleStartMessage:
-	ld a, [wBattleMode]
-	dec a
-	jr z, .wild
-
-	ld de, SFX_SHINE
-	call PlaySFX
-	call WaitSFX
-
-	ld c, 20
-	call DelayFrames
-
-	farcall Battle_GetTrainerName
-
-	ld hl, WantsToBattleText
-	jr .PlaceBattleStartText
-
-.wild
-	call BattleCheckEnemyShininess
-	jr nc, .not_shiny
-
-	xor a
-	ld [wNumHits], a
-	ld a, 1
-	ldh [hBattleTurn], a
-	ld a, 1
-	ld [wBattleAnimParam], a
-	ld de, ANIM_SEND_OUT_MON
-	call Call_PlayBattleAnim
-
-.not_shiny
-	farcall CheckSleepingTreeMon
-	jr c, .skip_cry
-
-	farcall CheckBattleScene
-	jr c, .cry_no_anim
-
-	hlcoord 12, 0
-	ld d, $0
-	ld e, ANIM_MON_NORMAL
-	predef AnimateFrontpic
-	jr .skip_cry ; cry is played during the animation
-
-.cry_no_anim
-	ld a, $f
-	ld [wCryTracks], a
-	ld a, [wTempEnemyMonSpecies]
-	call PlayStereoCry
-
-.skip_cry
-	ld a, [wBattleType]
-	cp BATTLETYPE_FISH
-	jr nz, .NotFishing
-
-	farcall StubbedTrainerRankings_HookedEncounters
-
-	ld hl, HookedPokemonAttackedText
-	jr .PlaceBattleStartText
-
-.NotFishing:
-	ld hl, PokemonFellFromTreeText
-	cp BATTLETYPE_TREE
-	jr z, .PlaceBattleStartText
-	ld hl, WildCelebiAppearedText
-	cp BATTLETYPE_CELEBI
-	jr z, .PlaceBattleStartText
-	ret ; Added to gain time at the start of battles.
-	;ld hl, WildPokemonAppearedText
-
-.PlaceBattleStartText:
-	push hl
-	farcall BattleStart_TrainerHuds
-	pop hl
-	call StdBattleTextbox
-
-	call IsMobileBattle2
-	ret nz
-
-	ld c, $2 ; start
-	farcall Mobile_PrintOpponentBattleMessage
-
 	ret
--- engine/battle/core_bis.asm
+++ engine/battle/core_bis.asm
@@ -1,3 +1,51 @@
+BattleIntro:
+	farcall StubbedTrainerRankings_Battles ; mobile
+	farcall ForceBattleAnimationStart
+	call LoadTrainerOrWildMonPic
+	xor a
+	ld [wTempBattleMonSpecies], a
+	ld [wBattleMenuCursorPosition], a
+	xor a
+	ldh [hMapAnims], a
+	farcall PlayBattleMusic
+	farcall ShowLinkBattleParticipants
+	farcall FindFirstAliveMonAndStartBattle
+	call DisableSpriteUpdates
+	farcall ClearBattleRAM
+	call InitEnemy
+	call BackUpBGMap2
+	ld b, SCGB_BATTLE_GRAYSCALE
+	call GetSGBLayout
+	ld hl, rLCDC
+	res rLCDC_WINDOW_TILEMAP, [hl] ; select vBGMap0/vBGMap2
+	farcall InitBattleDisplay
+	call BattleStartMessage
+	ld hl, rLCDC
+	set rLCDC_WINDOW_TILEMAP, [hl] ; select vBGMap1/vBGMap3
+	xor a
+	ldh [hBGMapMode], a
+	farcall EmptyBattleTextbox
+	hlcoord 9, 7
+	lb bc, 5, 11
+	call ClearBox
+	hlcoord 1, 0
+	lb bc, 4, 10
+	call ClearBox
+	call ClearSprites
+	ld a, [wBattleMode]
+	cp WILD_BATTLE
+	jr nz, .skip_enemy_hud_update
+
+	farcall UpdateEnemyHUD
+
+.skip_enemy_hud_update
+	ld a, $1
+	ldh [hBGMapMode], a
+	ret
+	
+
+
+
 ExitBattle:
 	call ForceBattleAnimationEnd
 	call .HandleEndOfBattle
@@ -26,6 +74,23 @@ ExitBattle:
 	farcall GivePokerusAndConvertBerries
 	ret
 	
+
+
+
+LoadTrainerOrWildMonPic:
+	ld a, [wOtherTrainerClass]
+	and a
+	jr nz, .Trainer
+	ld a, [wTempWildMonSpecies]
+	ld [wCurPartySpecies], a
+
+.Trainer:
+	ld [wTempEnemyMonSpecies], a
+	ret
+
+
+
+
 _StartAutomaticBattleWeather::
 	callfar GetAutomaticBattleWeather
 	ldh a, [hFarByte]
@@ -69,6 +134,9 @@ _StartAutomaticBattleWeather::
 
 INCLUDE "data/battle/automatic_weather_anims.asm"
 
+
+
+
 ForceBattleAnimationStart::
 	ld a, [wBattleType]
 	cp BATTLETYPE_SHINY
@@ -97,4 +165,235 @@ ForceBattleAnimationEnd::
 .restore_animation_options
 	ld a, [wOptionsBackup]
 	ld [wOptions], a
+	ret
+
+
+
+
+InitEnemy:
+	ld a, [wOtherTrainerClass]
+	and a
+	jr nz, InitEnemyTrainer ; trainer
+	;jp InitEnemyWildmon ; wild
+
+;InitEnemyWildmon:
+	ld a, WILD_BATTLE
+	ld [wBattleMode], a
+	farcall StubbedTrainerRankings_WildBattles
+	farcall LoadEnemyMon
+	ld hl, wEnemyMonMoves
+	ld de, wWildMonMoves
+	ld bc, NUM_MOVES
+	call CopyBytes
+	ld hl, wEnemyMonPP
+	ld de, wWildMonPP
+	ld bc, NUM_MOVES
+	call CopyBytes
+	ld hl, wEnemyMonDVs
+	predef GetUnownLetter
+	ld a, [wCurPartySpecies]
+	cp UNOWN
+	jr nz, .skip_unown
+	ld a, [wFirstUnownSeen]
+	and a
+	jr nz, .skip_unown
+	ld a, [wUnownLetter]
+	ld [wFirstUnownSeen], a
+.skip_unown
+	ld de, vTiles2
+	predef GetAnimatedFrontpic
+	xor a
+	ld [wTrainerClass], a
+	ldh [hGraphicStartTile], a
+	hlcoord 12, 0
+	lb bc, 7, 7
+	predef PlaceGraphic
+	ret
+
+
+
+
+InitEnemyTrainer:
+	ld [wTrainerClass], a
+	farcall StubbedTrainerRankings_TrainerBattles
+	xor a
+	ld [wTempEnemyMonSpecies], a
+	callfar GetTrainerAttributes
+	callfar ReadTrainerParty
+
+	; RIVAL1's first mon has no held item
+	ld a, [wTrainerClass]
+	cp RIVAL1
+	jr nz, .ok
+	xor a
+	ld [wOTPartyMon1Item], a
+
+.ok
+	ld de, vTiles2
+	callfar GetTrainerPic
+	xor a
+	ldh [hGraphicStartTile], a
+	dec a
+	ld [wEnemyItemState], a
+	hlcoord 12, 0
+	lb bc, 7, 7
+	predef PlaceGraphic
+	ld a, -1
+	ld [wCurOTMon], a
+	ld a, TRAINER_BATTLE
+	ld [wBattleMode], a
+
+	call IsGymLeader
+	jr nc, .done
+	xor a
+	ld [wCurPartyMon], a
+	ld a, [wPartyCount]
+	ld b, a
+.partyloop
+	push bc
+	ld a, MON_HP
+	call GetPartyParamLocation
+	ld a, [hli]
+	or [hl]
+	jr z, .skipfaintedmon
+	ld c, HAPPINESS_GYMBATTLE
+	callfar ChangeHappiness
+.skipfaintedmon
+	pop bc
+	dec b
+	jr z, .done
+	ld hl, wCurPartyMon
+	inc [hl]
+	jr .partyloop
+.done
+	ret
+
+
+
+
+IsKantoGymLeader:
+	ld hl, KantoGymLeaders
+	jr IsGymLeaderCommon
+	
+IsGymLeader:
+	ld hl, GymLeaders
+IsGymLeaderCommon:
+	push de
+	ld a, [wOtherTrainerClass]
+	ld de, 1
+	call IsInArray
+	pop de
+	ret
+
+INCLUDE "data/trainers/leaders.asm"
+
+
+
+BackUpBGMap2:
+	ldh a, [rSVBK]
+	push af
+	ld a, BANK(wDecompressScratch)
+	ldh [rSVBK], a
+	ld hl, wDecompressScratch
+	ld bc, $40 tiles ; vBGMap3 - vBGMap2
+	ld a, $2
+	call ByteFill
+	ldh a, [rVBK]
+	push af
+	ld a, $1
+	ldh [rVBK], a
+	ld de, wDecompressScratch
+	hlbgcoord 0, 0 ; vBGMap2
+	lb bc, BANK(BackUpBGMap2), $40
+	call Request2bpp
+	pop af
+	ldh [rVBK], a
+	pop af
+	ldh [rSVBK], a
+	ret
+
+
+
+
+BattleStartMessage:
+	ld a, [wBattleMode]
+	dec a
+	jr z, .wild
+
+	ld de, SFX_SHINE
+	call PlaySFX
+	call WaitSFX
+
+	ld c, 20
+	call DelayFrames
+
+	farcall Battle_GetTrainerName
+
+	ld hl, WantsToBattleText
+	jr .PlaceBattleStartText
+
+.wild
+	farcall BattleCheckEnemyShininess
+	jr nc, .not_shiny
+
+	xor a
+	ld [wNumHits], a
+	ld a, 1
+	ldh [hBattleTurn], a
+	ld a, 1
+	ld [wBattleAnimParam], a
+	ld de, ANIM_SEND_OUT_MON
+	farcall Call_PlayBattleAnim
+
+.not_shiny
+	farcall CheckSleepingTreeMon
+	jr c, .skip_cry
+
+	farcall CheckBattleScene
+	jr c, .cry_no_anim
+
+	hlcoord 12, 0
+	ld d, $0
+	ld e, ANIM_MON_NORMAL
+	predef AnimateFrontpic
+	jr .skip_cry ; cry is played during the animation
+
+.cry_no_anim
+	ld a, $f
+	ld [wCryTracks], a
+	ld a, [wTempEnemyMonSpecies]
+	call PlayStereoCry
+
+.skip_cry
+	ld a, [wBattleType]
+	cp BATTLETYPE_FISH
+	jr nz, .NotFishing
+
+	farcall StubbedTrainerRankings_HookedEncounters
+
+	ld hl, HookedPokemonAttackedText
+	jr .PlaceBattleStartText
+
+.NotFishing:
+	ld hl, PokemonFellFromTreeText
+	cp BATTLETYPE_TREE
+	jr z, .PlaceBattleStartText
+	ld hl, WildCelebiAppearedText
+	cp BATTLETYPE_CELEBI
+	jr z, .PlaceBattleStartText
+	ret ; Added to gain time at the start of battles.
+	;ld hl, WildPokemonAppearedText
+
+.PlaceBattleStartText:
+	push hl
+	farcall BattleStart_TrainerHuds
+	pop hl
+	call StdBattleTextbox
+
+	farcall IsMobileBattle2
+	ret nz
+
+	ld c, $2 ; start
+	farcall Mobile_PrintOpponentBattleMessage
+
 	ret
