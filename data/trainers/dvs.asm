4c749f9ac5b3c0cbc01a10012ee3629a8cb1d2f5
Damien Doury <damien.doury@hotmail.fr>
2022-12-02 12:10:34 +0100

    [edit] trainer edits here and there.

--- data/trainers/dvs.asm
+++ data/trainers/dvs.asm
@@ -2,32 +2,32 @@ TrainerClassDVs:
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	table_width 2, TrainerClassDVs
 	;  atk,def,spd,spc
-	dn  9, 10,  7,  7 ; FALKNER
-	dn  8,  8,  8,  8 ; WHITNEY
-	dn  9,  8,  8,  8 ; BUGSY
-	dn  9,  8,  8,  8 ; MORTY
-	dn  9,  8,  8,  8 ; PRYCE
-	dn  9,  8,  8,  8 ; JASMINE
-	dn  9,  8,  8,  8 ; CHUCK
-	dn  7, 12, 13, 13 ; CLAIR
+	dn 12, 12, 12, 12 ; FALKNER
+	dn 12, 12, 12, 12 ; WHITNEY
+	dn 12, 12, 12, 12 ; BUGSY
+	dn 12, 12, 12, 12 ; MORTY
+	dn 12, 12, 12, 12 ; PRYCE
+	dn 12, 12, 12, 12 ; JASMINE
+	dn 12, 12, 12, 12 ; CHUCK
+	dn 12, 12, 12, 12 ; CLAIR
 	dn 13, 13, 13, 13 ; RIVAL1
 	dn  9,  8,  8,  8 ; POKEMON_PROF
-	dn 13, 12, 13, 13 ; WILL
+	dn 14, 14, 14, 14 ; WILL
 	dn 13, 12, 13, 13 ; CAL
-	dn 13, 12, 13, 13 ; BRUNO
-	dn  7, 15, 13, 15 ; KAREN
-	dn 13, 12, 13, 13 ; KOGA
-	dn 13, 12, 13, 13 ; CHAMPION
-	dn  9,  8,  8,  8 ; BROCK
-	dn  7,  8,  8,  8 ; MISTY
-	dn  9,  8,  8,  8 ; LT_SURGE
+	dn 14, 14, 14, 14 ; BRUNO
+	dn 14, 14, 14, 14 ; KAREN
+	dn 14, 14, 14, 14 ; KOGA
+	dn 15, 15, 15, 15 ; CHAMPION
+	dn 12, 12, 12, 12 ; BROCK
+	dn 12, 12, 12, 12 ; MISTY
+	dn 12, 12, 12, 12 ; LT_SURGE
 	dn  9,  8,  8,  8 ; SCIENTIST
-	dn  7,  8,  8,  8 ; ERIKA
+	dn 12, 12, 12, 12 ; ERIKA
 	dn  9,  8,  8,  8 ; YOUNGSTER
 	dn  9,  8,  8,  8 ; SCHOOLBOY
 	dn  9,  8,  8,  8 ; BIRD_KEEPER
 	dn  5,  8,  8,  8 ; LASS
-	dn  9,  8,  8,  8 ; JANINE
+	dn 12, 12, 12, 12 ; JANINE
 	dn 13,  8, 12,  8 ; COOLTRAINERM
 	dn  7, 12, 12,  8 ; COOLTRAINERF
 	dn  6,  9, 12,  8 ; BEAUTY
@@ -47,7 +47,7 @@ TrainerClassDVs:
 	dn  9,  8,  8,  8 ; GUITARIST
 	dn 10,  8,  8,  8 ; HIKER
 	dn  9,  8,  8,  8 ; BIKER
-	dn  9,  8,  8,  8 ; BLAINE
+	dn 12, 12, 12, 12 ; BLAINE
 	dn  9,  8,  8,  8 ; BURGLAR
 	dn  9,  8,  8,  8 ; FIREBREATHER
 	dn  9,  8,  8,  8 ; JUGGLER
@@ -64,9 +64,9 @@ TrainerClassDVs:
 	dn  6,  8,  8, 10 ; KIMONO_GIRL
 	dn  6,  8, 10,  8 ; TWINS
 	dn  6, 13,  8,  8 ; POKEFANF
-	dn 15, 13, 13, 14 ; RED
-	dn 15, 15, 15, 15 ; BLUE
 	dn 15,  1, 15,  8 ; NURSE
+	dn 15, 15, 15, 15 ; RED
+	dn 14, 14, 14, 14 ; BLUE
 	dn  9,  8,  8,  8 ; OFFICER
 	dn  9, 10, 10, 10 ; SERGEANT
 	dn 12, 12, 12, 12 ; JENNY
