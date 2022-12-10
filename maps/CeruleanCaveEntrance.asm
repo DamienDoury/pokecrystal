246cbea8518e3e7602d5a546ed622d02f418f18e
Damien Doury <damien.doury@hotmail.fr>
2022-12-10 20:22:12 +0100

    [content] Red's mail added into cerulean cave entrance.

--- maps/CeruleanCaveEntrance.asm
+++ maps/CeruleanCaveEntrance.asm
@@ -19,6 +19,17 @@ CeruleanCaveEntrance_ExitScript:
 	warp CERULEAN_CITY, 0, 14
 	end
 
+CeruleanCaveMail:
+	jumptext CeruleanCaveMailText
+
+CeruleanCaveMailText:
+	text "There's a mail"
+	line "deposited on top"
+	cont "of the rock."
+
+	para "“Don't go in there."
+	line "- RED”"
+	done
 
 CeruleanCaveEntrance_ExitQuestionText:
 	text "You can see the"
@@ -31,12 +42,13 @@ CeruleanCaveEntrance_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 15,  3, CERULEAN_CAVE_B1F, 1
+	warp_event  7,  3, CERULEAN_CAVE_B1F, 1
 
 	def_coord_events
-	coord_event 15, 15, SCENE_DEFAULT, CeruleanCaveEntrance_ExitScript
-	coord_event 15, 15, SCENE_FINISHED, CeruleanCaveEntrance_ExitScript
+	coord_event  7, 15, SCENE_DEFAULT, CeruleanCaveEntrance_ExitScript
+	coord_event  7, 15, SCENE_FINISHED, CeruleanCaveEntrance_ExitScript
 
 	def_bg_events
+	bg_event  6,  6, BGEVENT_READ, CeruleanCaveMail
 
 	def_object_events
