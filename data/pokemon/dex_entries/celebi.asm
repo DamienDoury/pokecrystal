412e08a82936aac0bf19656523ece66094058663
Damien Doury <damien.doury@hotmail.fr>
2022-12-31 10:56:48 +0100

    [edit] Using Celebi's gold version pokedex entry.

--- data/pokemon/dex_entries/celebi.asm
+++ data/pokemon/dex_entries/celebi.asm
@@ -1,10 +1,10 @@
 	db "TIMETRAVEL@" ; species name
 	dw 200, 110 ; height, weight
 
-	db   "Revered as a"
-	next "guardian of the"
-	next "forest, CELEBI"
-
-	page "appears wherever"
-	next "beautiful forests"
-	next "exist.@"
+	db 	 "This #MON wan-"
+	next "ders across time."
+	next "Grass and trees"
+	
+	para "flourish in the"
+	next "forests in which"
+	next "it has appeared.@"
