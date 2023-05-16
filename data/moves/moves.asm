0786ad37f92b84b68815d705de2f954f11f9c5a4
Damien Doury <damien.doury@hotmail.fr>
2023-05-16 08:13:22 +0200

    [edit] updated the move Acid to later generations effect.

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -63,7 +63,7 @@ Moves:
 	move SUPERSONIC,   EFFECT_CONFUSE,             0, NORMAL,        	STATUS,		55, 20,   0
 	move SONICBOOM,    EFFECT_STATIC_DAMAGE,      20, NORMAL,        	SPECIAL,	90, 20,   0
 	move DISABLE,      EFFECT_DISABLE,             0, NORMAL,        	STATUS,		55, 20,   0
-	move ACID,         EFFECT_DEFENSE_DOWN_HIT,   40, POISON,       	SPECIAL,	100, 30,  10
+	move ACID,         EFFECT_SP_DEF_DOWN_HIT,   40, POISON,  	     	SPECIAL,	100, 30,  10
 	move EMBER,        EFFECT_BURN_HIT,           40, FIRE,         	SPECIAL,	100, 25,  10
 	move FLAMETHROWER, EFFECT_BURN_HIT,           95, FIRE,         	SPECIAL,	100, 15,  10
 	move MIST,         EFFECT_MIST,                0, ICE,          	STATUS,		100, 30,   0
