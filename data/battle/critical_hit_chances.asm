8eb93ae1c860c53d6a0742246607867d936ad51f
Damien Doury <damien.doury@hotmail.fr>
2023-05-23 00:17:21 +0200

    [feature] Critical Hit Chances matching gen 7 and up.

--- data/battle/critical_hit_chances.asm
+++ data/battle/critical_hit_chances.asm
@@ -1,8 +1,8 @@
 CriticalHitChances:
-	db 1 out_of 15 ;  0
+	db 1 out_of 24 ;  0
 	db 1 out_of 8  ; +1
-	db 1 out_of 4  ; +2
-	db 1 out_of 3  ; +3
-	db 1 out_of 2  ; +4
-	db 1 out_of 2  ; +5
-	db 1 out_of 2  ; +6
+	db 1 out_of 2  ; +2
+	db $ff  ; +3
+	db $ff  ; +4
+	db $ff  ; +5
+	db $ff  ; +6
