8e9b23c0cdb388b350d15642230dddc7dfefbfa9
Damien Doury <damien.doury@hotmail.fr>
2023-11-20 22:33:52 +0100

    [feature] Tiles keep animating while textbox is open.
    Thanks to https://github.com/pret/pokecrystal/wiki/Tips-and-tricks#animate-tiles-even-when-textboxes-are-open

--- home/video.asm
+++ home/video.asm
@@ -427,11 +427,11 @@ AnimateTileset::
 	ret z
 
 ; Back out if we're too far into VBlank
-	ldh a, [rLY]
-	cp LY_VBLANK
-	ret c
-	cp LY_VBLANK + 7
-	ret nc
+;	ldh a, [rLY]
+;	cp LY_VBLANK
+;	ret c
+;	cp LY_VBLANK + 7
+;	ret nc
 
 	ldh a, [hROMBank]
 	push af
