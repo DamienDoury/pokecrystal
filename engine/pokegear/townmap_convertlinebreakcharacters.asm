b2e763a7c4a0438f72129a86c05fa8b5ea5f31e3
Damien Doury <damien.doury@hotmail.fr>
2024-01-17 17:52:11 +0100

    [feature] Ability to add a dash+line break in the Pokégear map names.

--- engine/pokegear/townmap_convertlinebreakcharacters.asm
+++ engine/pokegear/townmap_convertlinebreakcharacters.asm
@@ -1,21 +1,44 @@
 TownMap_ConvertLineBreakCharacters:
-	ld hl, wStringBuffer1
+    ld hl, wStringBuffer1
+    ld c, STRING_BUFFER_LENGTH - 1
 .loop
-	ld a, [hl]
-	cp "@"
-	jr z, .end
-	cp "%"
-	jr z, .line_feed
-	cp "¯"
-	jr z, .line_feed
-	inc hl
-	jr .loop
+    ld a, [hl]
+    cp "@"
+    jr z, .end
+    cp "%"
+    jr z, .hyphen_break
+    cp "¯"
+    jr z, .line_feed
+    inc hl
+    dec c
+    jr .loop
+
+.hyphen_break
+    ld hl, wStringBuffer1
+    ld e, STRING_BUFFER_LENGTH
+    ld d, 0
+    add hl, de
+    ld d, h
+    ld e, l
+    dec hl
+
+.string_shift_loop
+    ld a, [hl]
+    ld [de], a
+    dec hl
+    dec de
+
+    dec c
+    jr nz, .string_shift_loop
+
+    ld [hl], "-"
+    inc hl
 
 .line_feed
-	ld [hl], "<LF>"
+    ld [hl], "<LF>"
 
 .end
-	ld de, wStringBuffer1
-	hlcoord 9, 0
-	call PlaceString
-	ret
+    ld de, wStringBuffer1
+    hlcoord 9, 0
+    call PlaceString
+    ret
