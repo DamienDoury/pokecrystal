28913a18396a253d1384ca3ab633c12124222924
Damien Doury <damien.doury@hotmail.fr>
2024-03-19 23:33:19 +0100

    [fix] jr -> jp.

--- engine/battle/ai/scoring.asm
+++ engine/battle/ai/scoring.asm
@@ -1160,15 +1160,8 @@ AI_Smart_SuperFang:
 	ret
 
 AI_Smart_Paralyze:
-; Electric types can't be paralyzed, so using a paralyze effect is pointless.
-	ld a, [wBattleMonType1] ; Note: the Ghost type is always the first one (either Misdreavus or Gastly's family).
-	cp ELECTRIC
-	jp z, AIDiscourageMove
-
-	ld a, [wBattleMonType2] ; Note: the Ghost type is always the first one (either Misdreavus or Gastly's family).
-	cp ELECTRIC
-	jr z, AIDiscourageMove
-
+; Electric types can't be paralyzed.
+; The paralyzing moves have been previously discarded by AI_Status.
 ; 50% chance to discourage this move if player's HP is below 25%.
 	call AICheckPlayerQuarterHP
 	jr nc, .may_discourage
@@ -3483,11 +3476,11 @@ AI_Status:
 
 	ld a, [wTypeMatchup]
 	and a
-	jr nz, .checkmove
+	jp nz, .checkmove
 
 .immune
 	call AIDiscourageMove
-	jr .checkmove
+	jp .checkmove
 
 
 AI_Risky:
