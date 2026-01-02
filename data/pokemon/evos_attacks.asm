274ac800ec7bff00e5a5a92fd83604e0dcca5557
Damien Doury <damien.doury@hotmail.fr>
2026-01-02 22:36:58 +0100

    [fix] Low level Noctowls don't know Sky Attack.

--- data/pokemon/evos_attacks.asm
+++ data/pokemon/evos_attacks.asm
@@ -2242,11 +2242,11 @@ HoothootEvosAttacks:
 
 NoctowlEvosAttacks:
 	db 0 ; no more evolutions
+	db 1, SKY_ATTACK
 	db 1, TACKLE
 	db 1, GROWL
 	db 1, FORESIGHT
 	db 1, PECK
-	db 1, SKY_ATTACK
 	db 6, FORESIGHT
 	db 11, PECK
 	db 16, HYPNOSIS
