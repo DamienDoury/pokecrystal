f972929680cc64a1f6288d6f2de9160a6fce05c9
Damien Doury <damien.doury@hotmail.fr>
2026-01-17 16:43:37 +0100

    [content] Greatest JIGGLYPUFF scene ever.

--- maps/PewterPokecenter1F.asm
+++ maps/PewterPokecenter1F.asm
@@ -14,10 +14,26 @@ PewterPokecenter1F_MapScripts:
 .PewterPokecenter1F_AntiSoftLock:
 	farsjump AntiSoftLockPokeball
 
+PewterPokecenterSleepingNPC:
+	showemote EMOTE_SLEEP, LAST_TALKED, 30
+	end
+
 PewterPokecenter1FNurseScript:
+	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_3
+	iffalse .heal
+
+	showemote EMOTE_SLEEP, LAST_TALKED, 45
+	pause 10
+	showemote EMOTE_SHOCK, LAST_TALKED, 10
+	clearevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_3
+	
+.heal
 	jumpstd PokecenterNurseScript
 
 PewterPokecenter1FTeacherScript:
+	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_2
+	iftrue PewterPokecenterSleepingNPC
+
 	jumptextfaceplayer PewterPokecenter1FTeacherText
 
 PewterJigglypuff:
@@ -31,11 +47,16 @@ PewterJigglypuff:
 	closetext
 	loadmem wMap3ObjectMovement, SPRITEMOVEDATA_STANDING_DOWN ; PEWTERPOKECENTER1F_JIGGLYPUFF
 	musicfadeout MUSIC_POKEMON_CENTER, 16
+	setval SPRITEMOVEDATA_STANDING_UP
+	writemem wMap2ObjectMovement
+	writemem wMap5ObjectMovement
 	showemote EMOTE_SLEEP, PLAYER, 75
 	pause 5
 	showemote EMOTE_SAD, PEWTERPOKECENTER1F_JIGGLYPUFF, 30
 	pause 10
 	showemote EMOTE_QUESTION, PLAYER, 10
+	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_3 ; Make the nurse sleep.
+	setevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_2 ; Make all other NPCs sleep.
 
 ; Make all party mons sleep, unless they already have a status ailment, or if they have fainted.
 FOR N, 1, PARTY_LENGTH + 1
@@ -68,10 +89,31 @@ JigglypuffDanceLastMovement:
 	step_sleep 40
 	step_end
 
+PewterPokecenter1FStompLeftMovement:
+	turn_step LEFT
+	step_bump
+	step_end
+
 PewterPokecenter1FBugCatcherScript:
+	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_2
+	iftrue .oh_no
+	
 	jumptextfaceplayer PewterPokecenter1FBugCatcherText
 
+.oh_no
+	applymovement PEWTERPOKECENTER1F_BUG_CATCHER, PewterPokecenter1FStompLeftMovement
+	pause 3
+
+	opentext
+	writetext PewterPokecenter1FBugCatcherOhNoText
+	waitbutton
+	closetext
+	end
+
 Chris:
+	checkevent EVENT_TEMPORARY_UNTIL_MAP_RELOAD_2
+	iftrue PewterPokecenterSleepingNPC
+	
 	faceplayer
 	opentext
 	trade NPC_TRADE_CHRIS
@@ -144,6 +186,17 @@ endc
 
 	done
 
+PewterPokecenter1FBugCatcherOhNoText:
+if DEF(_FR_FR)
+	text "Oh non RONDOUDOU!"
+	line "Ca suffit!"
+else
+	text "Oh no JIGGLYPUFF!"
+	line "Not again!"
+endc
+
+	done
+
 PewterPokecenter1F_MapEvents:
 	db 0, 0 ; filler
 
@@ -158,7 +211,7 @@ PewterPokecenter1F_MapEvents:
 
 	def_object_events
 	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, PewterPokecenter1FNurseScript, -1
-	object_event  8,  6, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterPokecenter1FTeacherScript, -1
+	object_event  9,  6, SPRITE_TEACHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PewterPokecenter1FTeacherScript, -1
 	object_event  1,  3, SPRITE_JIGGLYPUFF, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterJigglypuff, -1
 	object_event  2,  3, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, PewterPokecenter1FBugCatcherScript, -1
-	object_event  7,  2, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Chris, -1
+	object_event  8,  2, SPRITE_POKEFAN_M, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Chris, -1
